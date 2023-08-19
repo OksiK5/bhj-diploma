e49393ca830cdc2ae05d0cc16a306b1714ef0d70
KaminOks <oxi.k.nvrsk@mail.ru>
2023-08-19 19:32:23 +0300

    first commit TransactionsWidget.js

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -12,7 +12,8 @@ class TransactionsWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor( element ) {
-
+    this.element = element;
+    this.registerEvents();
   }
   /**
    * Регистрирует обработчики нажатия на
@@ -21,6 +22,19 @@ class TransactionsWidget {
    * экземпляра окна
    * */
   registerEvents() {
+    const createIncomeButton = this.element.querySelector('.create-income-button');
+    const createExpenseButton = this.element.querySelector('.create-expense-button');
 
+    createIncomeButton.addEventListener('click', (e) => {
+     // e.preventDefault();
+      let createIncome = App.getModal('createIncom');
+      createIncome.open();
+    });
+
+    createExpenseButton.addEventListener('click', (e) => {
+      //e.preventDefault();
+      let createExpense = App.getModal('createExpense');
+      createExpense.open();
+    });
   }
 }
