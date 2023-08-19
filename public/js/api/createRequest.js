9cc4bbee41a913214e5942a60751eecfa2a4f25b
KaminOks <oxi.k.nvrsk@mail.ru>
2023-08-19 19:24:38 +0300

    first commit api/createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,48 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
-};
+    const request = new XMLHttpRequest();
+    request.withCredentials = true;
+    request.responseType = options.responseType;
+  
+    const { method = 'GET', url, data, callback } = options;
+  
+    let requestData = null;
+  
+    if (method === 'GET') {
+      const queryString = new URLSearchParams(data).toString();
+      requestData = `${url}${queryString ? `?${queryString}` : ''}`;
+  
+      try {
+        if (requestData) {
+          request.open(method, requestData, true);
+          request.send();
+        }
+      } catch (e) {
+        callback(e);
+      }
+    } else {
+      const formData = new FormData();
+  
+      for (const key in data) {
+        formData.append(key, data[key]);
+      }
+  
+      try {
+        request.open(method, url, true);
+        request.send(formData);
+      } catch (e) {
+        callback(e);
+      }
+    }
+  
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === request.DONE && request.status === 200) {
+        const err = null;
+        const response = request.response;
+        callback(err, response);
+      }
+    });
+  
+    return request;
+  };
